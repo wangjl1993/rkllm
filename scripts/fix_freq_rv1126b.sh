4c0e13b1b8ba3d958434851126a695b617e8ffdc
yanghuacong <will.yang@rock-chips.com>
2025-06-25 20:38:50 +0800

    release v1.2.1

--- scripts/fix_freq_rv1126b.sh
+++ scripts/fix_freq_rv1126b.sh
@@ -1,24 +1,26 @@
+#!/system/bin/sh
 echo 1 > /sys/devices/system/cpu/cpu0/cpuidle/state1/disable
 echo 1 > /sys/devices/system/cpu/cpu1/cpuidle/state1/disable
 echo 1 > /sys/devices/system/cpu/cpu2/cpuidle/state1/disable
 echo 1 > /sys/devices/system/cpu/cpu3/cpuidle/state1/disable
 
 echo "NPU available frequencies:"
-cat /sys/class/devfreq/ff300000.npu/available_frequencies
+cat /sys/class/devfreq/22000000.npu/available_frequencies
 echo "Fix NPU max frequency:"
-echo 1000000000 > /sys/kernel/debug/rknpu/freq
-cat /sys/kernel/debug/rknpu/freq
+echo userspace > /sys/class/devfreq/22000000.npu/governor
+echo 950000000 > /sys/class/devfreq/22000000.npu/userspace/set_freq
+cat /sys/class/devfreq/22000000.npu/cur_freq
 
 echo "CPU available frequencies:"
 cat /sys/devices/system/cpu/cpufreq/policy0/scaling_available_frequencies
 echo "Fix CPU max frequency:"
-echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 2016000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
-cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq
+echo userspace > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo 1608000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
+cat /sys/devices/system/cpu/cpufreq/policy0/scaling_cur_freq
 
 echo "DDR available frequencies:"
 cat /sys/class/devfreq/dmc/available_frequencies
 echo "Fix DDR max frequency:"
 echo userspace > /sys/class/devfreq/dmc/governor
-echo 2112000000 > /sys/class/devfreq/dmc/userspace/set_freq
+echo 1332000000 > /sys/class/devfreq/dmc/userspace/set_freq
 cat /sys/class/devfreq/dmc/cur_freq
